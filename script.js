8ed848b9cf5400eff2b1e332546fd0f33006c296
Rishuraj Singh <102401151+rraawww10@users.noreply.github.com>
2025-01-01 12:54:54 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-// complete this js code
-function Person(name, age) {}
-
-function Employee(name, age, jobTitle) {}
-
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
